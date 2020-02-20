9aa85c791119252e8916a58d527ed9c61677b460
bguo1997 <57571367+bguo1997@users.noreply.github.com>
2020-02-19 22:19:12 -0500

    Add files via upload

--- hw4_test.py
+++ hw4_test.py
@@ -3,7 +3,7 @@ import unittest
 class TestHW(unittest.TestCase):
 
     def test_print_commits(self):
-        self.assertEqual(print_commits(get_commit('bguo1997')), "Repo: HW09 Number of commits: 1, Repo: ssw-567 Number of commits: 2, Repo: Triangle-567 Number of commits: 5, None")
+        self.assertEqual(print_commits(get_commit('bguo1997')), "Repo: HW09 Number of commits: 1, Repo: ssw-567 Number of commits: 2, Repo: Triangle-567 Number of commits: 5, Repo: GitHubApi567 Number of commits: 4, None")
 
 
 if __name__ == '__main__':
