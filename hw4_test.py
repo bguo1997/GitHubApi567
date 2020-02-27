92c50fea004541a72951c03464606a77495b42bb
bguo1997 <57571367+bguo1997@users.noreply.github.com>
2020-02-27 18:28:28 -0500

    change the test

--- hw4_test.py
+++ hw4_test.py
@@ -1,10 +1,9 @@
 from hw4 import get_commit
 import unittest
 class TestHW(unittest.TestCase):
-
     def test_print_commits(self):
-        self.assertEqual(get_commit('richkempinski'),
-                         "{'Mocks': 10, 'Project1': 2, 'hellogitworld': 30, 'helloworld': 6, 'threads-of-life': 1}")
+        expect = {'Mocks': 10, 'Project1': 2, 'hellogitworld': 30, 'helloworld': 6, 'threads-of-life': 1}
+        self.assertEqual(expect, get_commit("richkempinski"))
 
 
 if __name__ == '__main__':
